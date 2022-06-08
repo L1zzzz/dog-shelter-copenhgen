c539d44bf30cfd2b5ad2420a7cf9d6d51de3d44f
durelka <95026028+durelka@users.noreply.github.com>
2022-06-08 12:59:18 +0200

    added script

---
+++ script.js
@@ -0,0 +1,16 @@
+function buttonCrossing(x) {
+    x.classList.toggle("change");
+  }
+
+  function openMenu() {
+      document.getElementById("myDropdown").classList.toggle("open").toggle('change');
+      //document.getElementById('container').classList.toggle('change');
+  }
+
+  function addtext1(){
+    document.getElementById('beg_text').classList.toggle('hidden');
+  }
+
+  function addtext2(){
+    document.getElementById('int_text').classList.toggle('hidden');
+  }
